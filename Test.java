deaa0665a1e6d99df259cd4c9e9edb81ed3d638e
osmanseytablaev <osman.seytablaev@gmail.com>
2021-06-27 21:03:05 +0300

    Update

---
+++ Test.java
@@ -0,0 +1,16 @@
+class Phone {
+    String color = "Black";
+    int width = 25;
+    int height = 50;
+}
+
+class Iphone8 extends Phone {
+    int camera = 2;
+    int memoryGB = 64;
+    void update() {
+
+    }
+}
+
+public class Test {
+}
